7b97d2e9036b6054de804a5b572e0bef6d039ed2
mukhindev <mukhin.dev@gmail.com>
2020-04-05 13:47:31 +0300

    app.svelte tab to space

--- src/App.svelte
+++ src/App.svelte
@@ -8,45 +8,45 @@ let colors = ['#48B056', '#008A25', '#2B9D3D']
 
 <style>
 svg {
-	width: 100%;
-	background-color: rgb(196, 231, 182);
+  width: 100%;
+  background-color: rgb(196, 231, 182);
 }
 </style>
 
 <div class="container">
-	<div class="grid">
-		<div class="col-9 sm-12">
-			<svg viewBox="-100 -100 200 200">
-				<Box {position} {size} {colors} />
-			</svg>
-		</div>
-		<div class="col-3 sm-12">
-			<div>Позиция</div>
-			<label>
-				<span>x</span>
-				<input type="number" bind:value={position.x} />
-			</label>
-			<label>
-				<span>y</span>
-				<input type="number" bind:value={position.y} />
-			</label>
-			<label>
-				<span>z</span>
-				<input type="number" bind:value={position.z} />
-			</label>
-			<div class="mt-2">Габариты</div>
-			<label>
-				<span>x</span>
-				<input type="number" bind:value={size.x} />
-			</label>
-			<label>
-				<span>y</span>
-				<input type="number" bind:value={size.y} />
-			</label>
-			<label>
-				<span>z</span>
-				<input type="number" bind:value={size.z} />
-			</label>
-		</div>
-	</div>
+  <div class="grid">
+    <div class="col-9 sm-12">
+      <svg viewBox="-100 -100 200 200">
+        <Box {position} {size} {colors} />
+      </svg>
+    </div>
+    <div class="col-3 sm-12">
+      <div>Позиция</div>
+      <label>
+        <span>x</span>
+        <input type="number" bind:value={position.x} />
+      </label>
+      <label>
+        <span>y</span>
+        <input type="number" bind:value={position.y} />
+      </label>
+      <label>
+        <span>z</span>
+        <input type="number" bind:value={position.z} />
+      </label>
+      <div class="mt-2">Габариты</div>
+      <label>
+        <span>x</span>
+        <input type="number" bind:value={size.x} />
+      </label>
+      <label>
+        <span>y</span>
+        <input type="number" bind:value={size.y} />
+      </label>
+      <label>
+        <span>z</span>
+        <input type="number" bind:value={size.z} />
+      </label>
+    </div>
+  </div>
 </div>
